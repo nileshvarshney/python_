69aa814303b20fecf9da5ae4e83ea6bb39b20e9c
nileshvarshney <nilesh.varshney@yahoo.com>
2021-03-25 05:07:56 +0000

    test

--- Spark/dataframe/dataframe_basic.py
+++ Spark/dataframe/dataframe_basic.py
@@ -1,6 +1,7 @@
 import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, IntegerType, StringType, DateType, DoubleType
+from pyspark.sql import functions as F
 
 spark = SparkSession.builder.appName('Basics').getOrCreate()
 
@@ -57,8 +58,8 @@ daily_profit_sql = """
 """
 
 daily_store_profit = spark.sql(daily_profit_sql)
-daily_store_profit.show(3)
-
+#daily_store_profit.show(3)
+daily_store_profit.coalesce(1).write.format('com.databricks.spark.csv').save('../output/daily_store_profit.csv',header = 'true')
 
 # Location wise daily Profit
 location_wise_profit_sql = """
@@ -71,6 +72,41 @@ location_wise_profit_sql = """
     ORDER BY 3 DESC
 """
 
-location_wise_profit = spark.sql(location_wise_profit_sql)
-location_wise_profit.show(3)
 
+location_wise_profit = spark.sql(location_wise_profit_sql)
+location_wise_profit.coalesce(1).write.format('com.databricks.spark.csv').save('../output/location_wise_profit.csv',header = 'true')
+
+
+
+# Top Selling products in New York
+top_selling_prod_category = \
+df.filter(df.STORE_LOCATION == 'New York') \
+    .select(['PRODUCT_CATEGORY','MRP','CP','SP']) \
+    .groupBy(['PRODUCT_CATEGORY']) \
+    .agg(F.min('MRP').alias("MIM_MPR"), \
+        F.max('MRP').alias('MAX_MRP') ,\
+        F.min('CP').alias('MIN_CP'), \
+        F.max('CP').alias('MAX_CP'), \
+        F.sum('MRP').alias('SUM_MRP'), \
+        F.bround(F.sum("CP"),2).alias('SUM_CP'),\
+        F.bround(F.sum('SP'),2).alias('SUM_SP'), \
+        F.count('MRP').alias('COUNT_MRP')) \
+    .orderBy(F.col('COUNT_MRP').desc()).limit(5)
+# Write data back to output file
+top_selling_prod_category.coalesce(1).write.format('com.databricks.spark.csv').save('../output/top_selling_prod_category.csv',header = 'true')
+
+
+# Working with date
+date_data_output = \
+df.select( \
+    F.dayofyear(df['Date']).alias("DAY_OF_YEAR"),
+    F.month(df['Date']).alias("MONTH"),
+    F.dayofweek(df['Date']).alias("DAY_OF_WEEEK"),
+    F.dayofmonth(df['Date']).alias("DAY_OF_MONTH"),
+    F.year(df['Date']).alias("YEAR"),
+    )\
+    .distinct()
+
+# Write data back to output file
+#date_data_output.coalesce(1).write.csv('../output/date_data_output.csv')
+date_data_output.coalesce(1).write.format('com.databricks.spark.csv').save('../output/date_data_output.csv',header = 'true')
